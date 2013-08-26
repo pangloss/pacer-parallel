c053c9c545f9a5495a1eb2d5f2f02dc42d194e2e
Darrick Wiebe <dw@xnlogic.com>
2013-08-26 11:38:59 -0500

    consistent license

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   pacer.parallel "0.1.0-SNAPSHOT"
   :description "Leveraging core.async in Pacer"
   :url         "http://xnlogic.com"
-  :license     {:name "Eclipse Public License"
-                :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license     {:name "MIT"}
   :min-lein-version  "2.0.0"
   :dependencies   [[org.clojure/clojure "1.5.1"]
                    [core.async "0.1.0-SNAPSHOT"]
